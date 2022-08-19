bb5a112a1c23ca8e55455c80be3ac106181ac3e3
jefersonfranco <jefersonfranco@protonmail.com>
2022-08-19 17:21:39 -0300

    update almostIncreasingSequence.js

--- src/almostIncreasingSequence.js
+++ src/almostIncreasingSequence.js
@@ -24,3 +24,53 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test almostIncreasingSequence
+
+// alternative solution
+// function solution(sequence) {
+//     let bad = 0;
+//     for (let i = 1; i < sequence.length; i++) {
+//         if (sequence[i] <= sequence[i - 1]) {
+//             bad++;
+//             if (bad > 1) {
+//                 return false;
+//             }
+//             if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+// alternative solution
+// function solution(sequence) {
+//     let count = 0;
+//     for (let i = 0; i < sequence.length; i++) {
+//         if (sequence[i] <= sequence[i - 1]) {
+//             count++;
+//             if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return count <= 1;
+// }
+
+// alternative solution
+// function solution(sequence) {
+//     if(sequence.length <= 2) return true;
+//     let error = 0;
+//     for(let i = 0; i < sequence.length -1; i++) {
+//         if(sequence[i] >= sequence[i+1]) {
+//             let noStepBack = sequence[i-1] && sequence[i-1] >= sequence[i+1];
+//             let noStepForward = sequence[i+2] && sequence[i] >= sequence[i+2];
+//             if(i > 0 && noStepBack && noStepForward) {
+//                 error += 2;
+//             } else {
+//                 error++;
+//             }
+//         }
+//         if(error > 1) return false;
+//     }
+//     return true;
+// }
